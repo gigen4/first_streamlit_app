812e60b8a1529ba5b762eebaddb62430da76515e
gigen4 <130969098+gigen4@users.noreply.github.com>
2023-04-19 14:43:29 +0200

    Update streamlit_app.py
    inserito conduzione

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('ciao conduzione')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
